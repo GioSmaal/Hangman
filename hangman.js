5e00d0fe2230e47d7dc3e0497a7a47d826a6f6b5
i432855 <432855@student.fontys.nl>
2020-11-12 14:50:24 +0100

    Base game working

--- hangman.js
+++ hangman.js
@@ -18,6 +18,8 @@ function load() {
         'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
         't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
+
+    var count = -1;
     
     //make buttons
         for (var j = 0; j < alphabet.length; j++) {
@@ -28,9 +30,47 @@ function load() {
         
         var buttons = document.getElementById("buttons"); //reach html div
         buttons.appendChild(button); //assign button to html
-        buttons.addEventListener("click", checkLetter); //add function to the button
+        var individual = document.getElementById(alphabet[j]); 
+        individual.addEventListener("click", checkLetter);
     }
     function checkLetter() {
-        alert(button.id);
+        if (word.includes(this.id)) { //check if letter pressed is included in the word
+            
+        word.split(' '); //turn word into array
+            for (var k = 0; k < answerArray.length; k++) { 
+                if (word[k] == this.id) { //check if letter corresponds to button pressed               
+                    answerArray[k] = answerArray[k].replace(answerArray[k], this.id); //replace _ with letter
+                }
+            }
+        document.getElementById("word").innerHTML = "The word is " + answerArray.join(" "); //update text
+
+            if(answerArray.includes("_")) {
+
+            }
+            
+            else {               
+                document.getElementById("word").innerHTML = "The word is " + answerArray.join("");
+                document.getElementById("end").innerHTML = "You won!";
+                document.getElementById("reset").style.visibility = "visible";
+                
+            }
+        }
+        else {
+            count++;
+            
+            document.getElementById("hangman").src = count + ".jpg"; //update Hangman picture
+            
+            if (count == 6) {
+                document.getElementById("end").innerHTML = "You lost! The word was: " + word;
+                document.getElementById("reset").style.visibility = "visible";
+            }
+        }
+
+        this.style.visibility = "hidden";
     }
+
+    
+}
+function resetPage() {
+    location.reload();
 }
